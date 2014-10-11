260aa8d8617e56c5ee00188376419c283dcd74df
k-masuda <k-masuda@www8030up.sakura.ne.jp>
2014-10-11 15:18:30 +0900

    リファクタ

--- apps/frontend/modules/Page/actions/components.class.php
+++ apps/frontend/modules/Page/actions/components.class.php
@@ -4,9 +4,15 @@ class PageComponents extends sfComponents
 {
   public function executeListLatestNews()
   {
-    $this->pageList = PageTable::getInstance()->createQuery('p')
-      ->where('p.category = ?', 'news')
-      ->limit(5)
-      ->execute();
+    //$this->pageList = PageTable::getInstance()->createQuery('p')
+      //->where('p.category = ?', 'news')
+      //->limit(5)
+      //->execute();
+
+    $pager = new sfDoctrinePager('Page', 5);
+    $pager->setQuery(PageTable::queryLatestNews());
+    $pager->setPage(1);
+    $pager->init();
+    $this->latestNewsPager = $pager;
   }
 }
